77e549a5ae8d2d68e0f4232b7414ec26f138e495
PierceLe <haichau.hvsg@gmail.com>
2024-08-12 17:18:31 +1000

    chore: Finish spliting commandline

--- mysh.py
+++ mysh.py
@@ -1,4 +1,5 @@
 import signal
+import shlex
 
 
 # DO NOT REMOVE THIS FUNCTION!
@@ -9,14 +10,40 @@ def setup_signals() -> None:
     Setup signals required by this program.
     """
     signal.signal(signal.SIGTTOU, signal.SIG_IGN)
+    
+
+def split_arguments(command: str) -> list:
+    try:
+        command_argument = shlex.split(command)
+        return command_argument
+    except (ValueError):
+        return []
+
 
 
 def main() -> None:
     # DO NOT REMOVE THIS FUNCTION CALL!
     setup_signals()
-
-    # Start your code here!
-
+    while True:
+        try:
+            command = input(">> ")
+        except EOFError:
+            print()
+            break
+        except KeyboardInterrupt:
+            print()
+            continue
+        if command.strip() == "exit":
+            print("exit")
+            break
+        print(command)
+        command_argument: list = split_arguments(command)
+        if command_argument == []:
+            print("mysh: syntax error: unterminated quote")
+        else:
+            print(command_argument)
+            
+    
 
 if __name__ == "__main__":
     main()
