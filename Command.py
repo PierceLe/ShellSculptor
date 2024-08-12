23526714b4c13e7d99858b3d53a04857a25ab415
PierceLe <haichau.hvsg@gmail.com>
2024-08-12 18:44:50 +1000

    update: create an abstract class called Command

---
+++ Command.py
@@ -0,0 +1,19 @@
+from abc import ABC, abstractmethod
+
+
+class Command(ABC):
+    def __init__(self, argument: list):
+        self._argument = argument
+    
+    @property
+    def argument(self) -> list:
+        return self._argument
+
+    # @abstractmethod
+    # def validate_argument(self) -> bool:
+    #     pass
+        
+        
+    @abstractmethod
+    def execute(self):
+        pass
