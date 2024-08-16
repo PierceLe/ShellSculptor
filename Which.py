9853ad6a590fcd41d9d175725253183fb52fd12c
PierceLe <haichau.hvsg@gmail.com>
2024-08-16 15:41:17 +1000

    feat(which): implementing new command

---
+++ Which.py
@@ -0,0 +1,26 @@
+import os
+import sys
+from Command import Command
+
+
+class Which(Command):
+    def __init__(self, build_in_commands: list, argument: list):
+        super().__init__(argument)
+        self.build_in_commands = build_in_commands
+
+    def execute_file(self, command: str):
+        if command in self.build_in_commands:
+            return f"{command}: shell built-in command"
+        paths = os.getenv('PATH', os.defpath).split(os.pathsep)
+        for path in paths:
+            full_path = os.path.join(path, command)
+            if os.path.isfile(full_path) and os.access(full_path, os.X_OK):
+                return full_path
+        return f"{command}: command not found"
+
+    def execute(self):
+        commands: list = self._argument[1:]
+        if len(commands) == 0:
+            print(f"usage: which command ...", file=sys.stderr)
+        for command in commands:
+            print(self.execute_file(command))
