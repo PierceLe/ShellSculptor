7125392ecdc5042a96c37ad0be09d9a6275a1878
PierceLe <haichau.hvsg@gmail.com>
2024-08-17 23:04:08 +1000

    chore: start implement var command

--- Command.py
+++ Command.py
@@ -1,15 +1,19 @@
 from abc import ABC, abstractmethod
-from distutils.cmd import Command
+import re
 
 
 class Command(ABC):
     def __init__(self, argument: list):
         self._argument = argument
 
+    def valid_variable_name(self, variable_name: str) -> bool:
+        pattern = r'^[A-Za-z0-9_]+$'
+        return bool(re.match(pattern, variable_name))
+
     @property
     def argument(self) -> list:
         return self._argument
 
     @abstractmethod
-    def execute(self):
+    def execute(self) -> None:
         pass
---
+++ Var.py
@@ -0,0 +1,12 @@
+import os
+import sys
+import re
+from Command import Command
+
+
+class Var(Command):
+    def execute(self):
+        if len(self._argument) == 1:
+            pass
+
+        pass
